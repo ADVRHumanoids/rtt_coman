589d9c68c3ba198686075076a944931b7d220bfa
Enrico Mingo <enrico.mingo@iit.it>
2016-11-24 16:30:04 +0100

    Update

--- include/rtt_coman.hpp
+++ include/rtt_coman.hpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016 Cogimon
+ * Author: Enrico Mingo Hoffman
+ * email:  enrico.mingo@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 #ifndef _RTT_COMAN_HPP_
 #define _RTT_COMAN_HPP_
 
